a434957f1bd92dd88f19dedf5d00dc86f02d577c
Tomas Valasek <jajsemtomas@gmail.com>
2019-08-06 18:19:45 +0200

    Grafky test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import logo from './img/logo.svg';
 import './css/App.css';
+import ReactStoreIndicator from 'react-score-indicator'
+import AreaChart from './components/AreaChart';
 
 function App() {
+
+  let maxValue = 70;
+  let score = 40;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, react is rdy to be played with
-        </p>
+
+        <ReactStoreIndicator
+          value={score}
+          maxValue={maxValue}
+          width={200}
+          lineWidth={30}
+          lineGap={7}
+        />
+        <AreaChart/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/AreaChart.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Chart from "react-apexcharts";
+
+class AreaChart extends React.Component {
+      
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+
+        xaxis: {
+          type: 'datetime',
+          categories: ["2018-01-01T00:00:00", "2018-02-01T00:00:00", "2018-03-01T00:00:00",
+            "2018-04-01T00:00:00", "2018-05-01T00:00:00", "2018-06-01T00:00:00",
+            "2018-07-01T00:00:00","2018-08-01T00:00:00", "2018-09-01T00:00:00", "2018-10-01T00:00:00",
+            "2018-11-01T00:00:00", "2018-12-01T00:00:00"
+          ],
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy'
+          },
+        }
+      },
+      series: [{
+        name: '2018',
+        data: [31, 40, 28, 51, 42, 80, 70, 31, 40, 28, 51, 42]
+      }, {
+        name: '2019',
+        data: [11, 32, 45, 32, 34, 52, 41, 11, 32, 45, 32, 34]
+      }],
+    }
+  }
+
+  render() {
+    return (
+      
+
+      <div id="chart">
+        <Chart options={this.state.options} series={this.state.series} type="area" height="350" width="500" />
+      </div>
+
+
+    );
+  }
+}
+
+export default AreaChart;
